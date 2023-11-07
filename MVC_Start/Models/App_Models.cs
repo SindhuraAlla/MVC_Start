86183f4f1df0bb111378d73b7e95444870a9f33b
KrishnaKSD3004 <142905291+KrishnaKSD3004@users.noreply.github.com>
2023-11-07 03:33:59 -0500

    Sync models from others

--- MVC_Start/Models/App_Models.cs
+++ MVC_Start/Models/App_Models.cs
@@ -1,24 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVC_Start.Models
 {
-  public class GuestContact
+  public class Product
   {
+    public string Id {get; set;}
     public string Name { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
+    public string Description { get; set; }
+    public ICollection<SubProduct> SubProducts { get; set; }
   }
 
-  public class ChartModel
+  public class SubProduct
   {
-    public string ChartType { get; set; }
-    public string Labels { get; set; }
-    public string Data { get; set; }
-    public string Title { get; set; }
+    public string Id { get; set; }
+    public string Name { get; set; }
+
+    [ForeignKey("Product")]
+    public string ProductId { get; set; }
+
+    public Product Product { get; set; }
+    public ICollection<Complaint> Complaints { get; set; }
   }
 
-  
+  public class Company
+  {
+      public string Id { get; set; }
+      public string Name { get; set; }
+      public ICollection<Complaint> Complaints { get; set; }
+  }
+
+  public class Complaint
+  {
+      public string ComplaintId { get; set; }
+      public string ComplaintWhatHappened { get; set; }
+      public DateTime DateSentToCompany { get; set; }
+      public string IssueName { get; set; }
+      public DateTime DateReceived { get; set; }
+      public string Timely { get; set; }
+      public string ConsumerConsentProvided { get; set; }
+      public string CompanyResponse { get; set; }
+      public string SubmittedVia { get; set; }
+      public string ConsumerDisputed { get; set; }
+      public string SubIssue { get; set; }
+      public string State { get; set; }
+      public string ZipCode { get; set; }
+
+      [ForeignKey("Company")]
+      public string CompanyId { get; set; }
+      [ForeignKey("SubProduct")]
+      public string SubProductId { get; set; }
+      public Company Company { get; set; }
+      public SubProduct SubProduct { get; set; }
+  }
 }
